f1208f7615a57d692f62a5ff63a6534a75fe83f1
kydef <stepan.fedyk@gmail.com>
2025-01-24 16:31:13 +0200

    feat: implement multiple variants order in add-to-checkout endpoint

--- src/app/pdf/add-to-checkout.handler.ts
+++ src/app/pdf/add-to-checkout.handler.ts
@@ -11,17 +11,33 @@ import { getPrisma, getSaleorClient } from "../connections";
 import { sendData, sendInternalError, sendNotFound, sendValidationError } from "../api.utils";
 import _ from "lodash";
 
+interface AddPdfToCheckoutBody {
+  // The channel to use for the checkout
+  channel: string; 
+  // The ID of the checkout to add the PDF to
+  checkoutId: string; 
+  // The slug of the PrintProduct to use
+  slug: string; 
+  // The hash of the uploaded PDF file
+  hash: string; 
+  // The variants of the printed PDF to add
+  variants: { 
+    // The ID of the product variant to use for the cover
+    coverVariantId: string; 
+    // The color of the cover text
+    coverTextColor: string; 
+    // The pages of the PDF that should be printed in color
+    coloredPages: number[]; 
+    // The quantity of the printed PDF to add
+    quantity: number; 
+  }[];
+}
+
 /**
  * POST /api/pdf/add-to-checkout
  *
  * Adds a printed PDF to a checkout.
- * @param req.body.hash - The hash of the uploaded PDF file.
- * @param req.body.checkoutId - The ID of the checkout to add the PDF to.
- * @param req.body.coloredPages - The pages of the PDF that should be printed in color.
- * @param req.body.slug - The slug of the PrintProduct to use.
- * @param req.body.coverVariantId - The ID of the product variant to use for the cover.
- * @param req.body.channel - The channel to use for the checkout.
- * @param req.body.quantity - The quantity of the printed PDF to add.
+ * @param req.body - The request body of type AddPdfToCheckoutBody containing the checkout ID, the hash of the PDF file, and the variants to add.
  * @returns Nothing if successful.
  */
 export default async function AddPdfToCheckoutHandler(
@@ -31,22 +47,18 @@ export default async function AddPdfToCheckoutHandler(
   if (req.method === 'OPTIONS') return res.status(200).send("");
   if (req.method !== "POST") return sendNotFound(res, "Method Not Allowed");
 
-  const body = req.body;
+  // Validate
+  const body = req.body as AddPdfToCheckoutBody;
   if (!validate(res, body)) return;
 
   // Get args
-  const hash: string = body.hash;
-  const checkoutId: string = body.checkoutId;
-  const coloredPages: number[] = body.coloredPages;
-  const coverVariantId: string = body.coverVariantId;
-  const slug: string = body.slug;
-  const channel: string = body.channel;
-  const quantity: number = body.quantity;
+  const { channel, checkoutId, slug, hash, variants } = body;
 
   // Fetch the uploaded file from the database
   const prisma = getPrisma();
   const uploadedFile = await prisma.uploadedFile.findUnique({ where: { hash } });
   if (!uploadedFile?.pageCount) return sendNotFound(res, "File not found");
+  if (uploadedFile.pageCount < 1) return sendInternalError(res, "Invalid page count");
 
   // Fetch print product
   const printProduct = await prisma.printProducts.findUnique({ where: { slug } });
@@ -66,8 +78,8 @@ export default async function AddPdfToCheckoutHandler(
     .toPromise();
   const products = productsResponse.data?.products?.edges?.map(({ node }) => node) || [];
   if (products?.length !== 2) return sendNotFound(res, "Product not found");
-
-  // Get variants
+  
+  // Find cover and page products
   const coverProduct = products.find((product) => product.id === printProduct.coverProductId);
   if (!coverProduct) return sendNotFound(res, "Cover product not found");
   const pageProduct = products.find((product) => product.id === printProduct.pageProductId);
@@ -76,20 +88,47 @@ export default async function AddPdfToCheckoutHandler(
   if (!colorPageVariant) return sendNotFound(res, "Colored page variant not found");
   const grayscalePageVariant = pageProduct.variants?.find((variant) => variant.id === printProduct.grayscalePageVariantId);
   if (!grayscalePageVariant) return sendNotFound(res, "Grayscale page variant not found"); 
-  const coverVariant = coverProduct.variants?.find((variant) => variant.id === coverVariantId);
-  if (!coverVariant) return sendNotFound(res, "Cover variant not found");
 
-  // Calculate
+  // Get colored and grayscale page prices
   const coloredPagePrice = colorPageVariant.pricing?.price?.gross.amount || 0;
   const grayscalePagePrice = grayscalePageVariant.pricing?.price?.gross.amount || 0;
-  const coverPrice = coverVariant.pricing?.price?.gross.amount || 0;
-  if (uploadedFile.pageCount < 1) return sendInternalError(res, "Invalid page count");
-  const coloredPagesCount = coloredPages.length;
-  const grayscalePagesCount = uploadedFile.pageCount - coloredPages.length;
-  if (grayscalePagesCount < 0) return sendInternalError(res, "Invalid colored page count");
-  const coloredPrice = coloredPagesCount * coloredPagePrice;
-  const grayscalePrice = grayscalePagesCount * grayscalePagePrice;
-  const totalPrice = coverPrice + coloredPrice + grayscalePrice;
+
+  // Find and calculate checkout lines
+  const checkoutLines = [];
+  for (const variant of variants) {
+    const coverVariant = coverProduct.variants?.find((v) => v.id === variant.coverVariantId);
+    if (!coverVariant) return sendNotFound(res, `Cover variant with id: ${variant.coverVariantId} variant not found`);
+
+    const coverPrice = coverVariant.pricing?.price?.gross?.amount || 0;
+    const coloredPagesCount = variant.coloredPages.length;
+    const grayscalePagesCount = uploadedFile.pageCount - coloredPagesCount;
+
+    if (grayscalePagesCount < 0) return sendInternalError(res, "Invalid colored page count");
+    const coloredPrice = coloredPagesCount * coloredPagePrice;
+    const grayscalePrice = grayscalePagesCount * grayscalePagePrice;
+    const totalPrice = coverPrice + coloredPrice + grayscalePrice;
+
+    checkoutLines.push({
+      quantity: variant.quantity,
+      variantId: variant.coverVariantId,
+      forceNewLine: true,
+      price: totalPrice,
+      metadata: [
+        {
+          key: "hash",
+          value: uploadedFile.hash,
+        },
+        {
+          key: "coloredPages",
+          value: variant.coloredPages.join(","),
+        },
+        {
+          key: "coverTextColor",
+          value: variant.coverTextColor,
+        },
+      ],
+    });
+  } 
 
   // Add to checkout
   const checkoutResponse = await gqlClient
@@ -97,24 +136,7 @@ export default async function AddPdfToCheckoutHandler(
       UntypedCheckoutLinesAddDocument,
       {
         id: checkoutId,
-        lines: [
-          {
-            quantity,
-            variantId: coverVariantId,
-            forceNewLine: true,
-            price: totalPrice,
-            metadata: [
-              {
-                key: "filePrintHash",
-                value: uploadedFile.hash,
-              },
-              {
-                key: "coloredPages",
-                value: coloredPages.join(","),
-              },
-            ],
-          },
-        ],
+        lines: checkoutLines,
       }
     )
     .toPromise();
@@ -125,38 +147,45 @@ export default async function AddPdfToCheckoutHandler(
   return sendData(res, null);
 }
 
-function validate(res: NextApiResponse, body: any): boolean {
-  if (!_.isString(body?.hash)) {
-    sendValidationError(res, "Invalid hash");
+function validate(res: NextApiResponse, body: AddPdfToCheckoutBody): boolean {
+  if (!_.isString(body?.channel)) {
+    sendValidationError(res, "Invalid channel");
     return false;
   }
   if (!_.isString(body?.checkoutId)) {
     sendValidationError(res, "Invalid checkoutId");
     return false;
   }
-  if (!_.isArray(body?.coloredPages)) {
-    sendValidationError(res, "Invalid coloredPages");
-    return false;
-  }
-  if (!_.isString(body?.coverVariantId)) {
-    sendValidationError(res, "Invalid coverVariantId");
-    return false;
-  }
   if (!_.isString(body?.slug)) {
     sendValidationError(res, "Invalid slug");
     return false;
   }
-  if (!_.isString(body?.channel)) {
-    sendValidationError(res, "Invalid channel");
+  if (!_.isString(body?.hash)) {
+    sendValidationError(res, "Invalid hash");
     return false;
   }
-  if (!_.isNumber(body?.quantity)) {
-    sendValidationError(res, "Invalid quantity");
+  if (!_.isArray(body?.variants) || !body.variants.every(_.isObject) || _.isEmpty(body.variants)) {
+    sendValidationError(res, "Invalid variants, must be an array of objects (not empty)");
     return false;
   }
-  if (!body.coloredPages.every((page: any) => _.isNumber(page))) {
-    sendValidationError(res, "Invalid coloredPages");
-    return false;
+  for (const variant of body?.variants) {
+    if (!_.isString(variant?.coverVariantId)) {
+      sendValidationError(res, "Invalid coverVariantId");
+      return false;
+    }
+    if (!_.isString(variant?.coverTextColor)) {
+      sendValidationError(res, "Invalid coverTextColor");
+      return false;
+    }
+    if (!_.isArray(variant?.coloredPages) || !variant.coloredPages.every(_.isNumber)) {
+      sendValidationError(res, "Invalid coloredPages, must be an array of numbers");
+      return false;
+    }
+    if (!_.isNumber(variant?.quantity)) {
+      sendValidationError(res, "Invalid quantity, must be a number");
+      return false;
+    }
   }
+
   return true;
 }
